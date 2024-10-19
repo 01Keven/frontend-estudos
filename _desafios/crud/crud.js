960b470eca4aa01b5f4805b4875c19264eb7a38f
01Keven <kevenchristiancode@gmail.com>
2024-10-18 23:12:06 -0300

    CRUD - salvando dados dos usuarios no localStorage

--- _desafios/crud/crud.js
+++ _desafios/crud/crud.js
@@ -1,2 +1,33 @@
+let modalCadastrar = document.querySelector('.cadastrar-container')
 
-let dadosUsuarios = JSON.parse(localStorage.getItem('dados')) || []
+modalCadastrar.style.display = 'none'
+
+// Eventos de botões
+
+document.getElementById('btnfechar').addEventListener('click', () => {
+    modalCadastrar.style.display = "none"
+})
+
+document.getElementById('btnmcadastrar').addEventListener('click', () => {
+    modalCadastrar.style.display = 'block'
+})
+
+// funções
+
+// Pegar o dados dos usuarios
+function getUsers() {
+    const users = localStorage.getItem('users')
+    // se users nao forem nulo converte os dados de string para um array de objetos, caso seja nulo retorna um array nulo
+    return users ? JSON.parse(users) : []
+}
+
+// salvar o array de users no localStorage
+function saveUsers(users) {
+    // converter array de objetos em string
+    localStorage.setItem('users', JSON.stringify(users))
+}
+
+function addUsers(nome,endereco, email, nascimento) {
+    const users = getUsers()
+    
+}
